ca29bd81d425a167d800e1b07fd49b891b91a86a
IAFahim <ishtiaq.fahim@northsouth.edu>
2022-09-21 18:16:48 +0600

    Its me Mario

---
+++ Codeforces Round #719 (Div. 3)/A.cs
@@ -0,0 +1,33 @@
+﻿namespace cp_with_cs.Codeforces_Round__719__Div._3_;
+
+public static class A
+{
+    static void Main()
+    {
+        var T = Convert.ToInt32(Console.ReadLine());
+        for (int t = 0; t < T; t++)
+        {
+            var _ = Convert.ToInt32(Console.ReadLine());
+            var str = Console.ReadLine();
+            var ans = "YES";
+            Boolean[] arr = new Boolean[128];
+            arr[str![0]] = true;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i - 1] != str[i])
+                {
+                    if (arr[str[i]])
+                    {
+                        ans = "No";
+                        break;
+                    }
+
+                    arr[str[i]] = true;
+                }
+            }
+
+            Console.WriteLine(ans);
+            
+        }
+    }
+}
---
+++ Codeforces Round #725 (Div. 3)/A.cs
@@ -0,0 +1,9 @@
+﻿namespace cp_with_cs.Codeforces_Round__725__Div._3_;
+
+public class A
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hello");
+    }
+}
